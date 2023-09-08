075793a8541e9460d2d942551c24b7797e4c3cc0
OlwethuM30 <olwethungqola@gmail.com>
2023-09-08 01:28:35 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
 * main - this is the main function
 *
-* Return: always 0
+* Return: Always 0 (Success) 
 */
-
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building  a multilingual puzzle);
+	puts("\"Programming is like building  a multilingual puzzle");
 	return (0);
 }
